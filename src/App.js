bf113cd62831bfd6ee59e3c5c1864940413fa8d9
Kunto Baskoro <kuntobaskoro@host-66-96-238-51.myrepublic.co.id>
2019-02-03 14:14:31 +0700

    Tes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Bersama Murat Owkey
+            Tes
           </a>
         </header>
       </div>
